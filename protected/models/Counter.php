d0cd6c4bc5401dc5ec0fe01aa1c877a1afa38c16
LBS IT <percylee@lbsgroup.com.hk>
2020-10-07 15:36:59 +0800

    同步

--- protected/models/Counter.php
+++ protected/models/Counter.php
@@ -52,6 +52,36 @@ class Counter {
 		
 		return $rtn;
 	}
+
+	public static function countPayrollAppr() {
+		$rtn = 0;
+		$wf = new WorkflowPayroll;
+		$wf->connection = Yii::app()->db;
+		$list1 = $wf->getPendingRequestIdList('PAYROLL', 'PA', Yii::app()->user->id);
+		$list2 = $wf->getPendingRequestIdList('PAYROLL', 'PB', Yii::app()->user->id);
+		$list3 = $wf->getPendingRequestIdList('PAYROLL', 'PC', Yii::app()->user->id);
+		$list4 = $wf->getPendingRequestIdList('PAYROLL', 'P1', Yii::app()->user->id);
+		$list5 = $wf->getPendingRequestIdList('PAYROLL', 'P2', Yii::app()->user->id);
+		$items1 = empty($list1) ? array() : explode(',',$list1);
+		$items2 = empty($list2) ? array() : explode(',',$list2);
+		$items3 = empty($list3) ? array() : explode(',',$list3);
+		$items4 = empty($list4) ? array() : explode(',',$list4);
+		$items5 = empty($list5) ? array() : explode(',',$list5);
+		$rtn = count($items1) + count($items2) + count($items3) + count($items4) + count($items5);
+		
+		return $rtn;
+	}
+
+	public static function countPayroll() {
+		$rtn = 0;
+		$wf = new WorkflowPayroll;
+		$wf->connection = Yii::app()->db;
+		$list = $wf->getPendingRequestIdList('PAYROLL', 'PS', Yii::app()->user->id);
+		$items = empty($list) ? array() : explode(',',$list);
+		$rtn = count($items);
+		
+		return $rtn;
+	}
 }
 
 ?>
---
+++ protected/models/PayrollApprList.php
@@ -0,0 +1,99 @@
+<?php
+
+class PayrollApprList extends CListPageModel
+{
+	public function attributeLabels()
+	{
+		return array(	
+			'year_no'=>Yii::t('report','Year'),
+			'month_no'=>Yii::t('report','Month'),
+			'city_name'=>Yii::t('misc','City'),
+			'wfstatusdesc'=>Yii::t('misc','Status'),
+			'file1countdoc'=>Yii::t('trans','Files'),
+		);
+	}
+	
+	public function retrieveDataByPage($pageNum=1, $type='P')
+	{
+		$wf = new WorkflowPayroll;
+		$wf->connection = Yii::app()->db;
+		$list1 = $wf->getPendingRequestIdList('PAYROLL', 'PA', Yii::app()->user->id);
+		$list2 = $wf->getPendingRequestIdList('PAYROLL', 'PB', Yii::app()->user->id);
+		$list3 = $wf->getPendingRequestIdList('PAYROLL', 'PC', Yii::app()->user->id);
+		$list4 = $wf->getPendingRequestIdList('PAYROLL', 'P1', Yii::app()->user->id);
+		$list5 = $wf->getPendingRequestIdList('PAYROLL', 'P2', Yii::app()->user->id);
+		$list = '0';
+		if (!empty($list1)) $list = $list1;
+		if (!empty($list2)) $list = $list=='0' ? $list2 : $list.','.$list2;
+		if (!empty($list3)) $list = $list=='0' ? $list3 : $list.','.$list3;
+		if (!empty($list4)) $list = $list=='0' ? $list4 : $list.','.$list4;
+		if (!empty($list5)) $list = $list=='0' ? $list5 : $list.','.$list5;
+		
+		$suffix = Yii::app()->params['envSuffix'];
+		$city = Yii::app()->user->city_allow();
+		$version = Yii::app()->params['version'];
+		$cityarg = ($version=='intl' ? 'a.city,' : '');
+		$sql1 = "select a.*, b.name as city_name, 
+					docman$suffix.countdoc('PAYFILE1',a.id) as file1countdoc,
+					workflow$suffix.RequestStatusDesc($cityarg 'PAYROLL',a.id,a.lcd) as wfstatusdesc
+				from acc_payroll_file_hdr a, security$suffix.sec_city b 
+				where a.city in ($city) and a.city=b.code 
+				and a.id in ($list)
+			";
+		$sql2 = "select count(a.id)
+				from acc_payroll_file_hdr a, security$suffix.sec_city b 
+				where a.city in ($city) and a.city=b.code 
+				and a.id in ($list)
+			";
+
+		$clause = "";
+		if (!empty($this->searchField) && !empty($this->searchValue)) {
+			$svalue = str_replace("'","\'",$this->searchValue);
+			switch ($this->searchField) {
+				case 'year_no':
+					$clause .= General::getSqlConditionClause('a.year_no', $svalue);
+					break;
+				case 'month_no':
+					$clause .= General::getSqlConditionClause('a.month_no', $svalue);
+					break;
+				case 'city_name':
+					$clause .= General::getSqlConditionClause('b.name', $svalue);
+					break;
+			}
+		}
+		
+		$order = "";
+		if (!empty($this->orderField)) {
+			$order .= " order by ".$this->orderField." ";
+			if ($this->orderType=='D') $order .= "desc ";
+		} else
+			$order = " order by a.year_no desc, a.month_no desc, a.city";
+
+		$sql = $sql2.$clause;
+		$this->totalRow = Yii::app()->db->createCommand($sql)->queryScalar();
+		
+		$sql = $sql1.$clause.$order;
+		$sql = $this->sqlWithPageCriteria($sql, $this->pageNum);
+		$records = Yii::app()->db->createCommand($sql)->queryAll();
+		
+		$list = array();
+		$this->attr = array();
+		if (count($records) > 0) {
+			foreach ($records as $k=>$record) {
+				$this->attr[] = array(
+						'id'=>$record['id'],
+						'year_no'=>$record['year_no'],
+						'month_no'=>$record['month_no'],
+						'city'=>$record['city'],
+						'city_name'=>$record['city_name'],
+						'wfstatusdesc'=>(empty($record['wfstatusdesc'])?Yii::t('misc','Draft'):$record['wfstatusdesc']),
+						'file1countdoc'=>$record['file1countdoc'],
+				);
+			}
+		}
+		$session = Yii::app()->session;
+		$session['criteria_xs06'] = $this->getCriteria();
+		return true;
+	}
+
+}
