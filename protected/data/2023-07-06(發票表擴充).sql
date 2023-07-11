88020d5f5cbdac1feec9df4b392fe8b3f2653488
shenchao <shenchao@lbsgroup.com.cn>
2023-07-11 09:11:16 +0800

    發票表添加賬單編號、付款週期字段

--- protected/data/2023-07-06(發票表擴充).sql
+++ protected/data/2023-07-06(發票表擴充).sql
@@ -3,3 +3,4 @@
 -- Table structure for acc_invoice
 -- ----------------------------
 ALTER TABLE acc_invoice ADD COLUMN payment_method varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '付款週期' AFTER payment_term;
+ALTER TABLE acc_invoice ADD COLUMN number_no varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '賬單編號' AFTER payment_term;
